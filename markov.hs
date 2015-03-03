d543dc4e6ce79cc174f1af9df0010a11999152ee
Kevin Vollmer <works.kvollmer@gmail.com>
2015-03-03 11:18:20 -0500

    rand structure somewhat forming

--- markov.hs
+++ markov.hs
@@ -1,15 +1,26 @@
 import System.IO
+import System.Random (randomRIO)
 
 data Markov = Markov {
             key :: (String,String),
             value :: [String]
             }
-            deriving Show
 type MarkovChain = [Markov]
 
 instance Eq (Markov) where
     (Markov (a1,b1) _ ) == (Markov (a2,b2) _ ) =
       (a1 == a2) && (b1 == b2)
+
+instance Show (Markov) where
+    show a = (fst $ key a) ++ " " ++ (snd $ key a)
+
+pick :: [a] -> IO a
+pick xs = randomRIO (0, length xs - 1) >>= return . (xs !!)
+
+prntMarkov :: Markov -> IO String
+prntMarkov x = do
+    m <- pick $ value x
+    return ((show x) ++ " " ++ m)
 
 chain :: Markov -> Markov -> Markov
 chain x y = Markov (key x) (value x ++ value y)
@@ -29,9 +40,16 @@ grpMarkov (x:xs)
 
 --loop markovchain
 --print key and value
---take second key and value and find in list
+--recurse second key and value in list; decrement count
+--repeat until count is 0
+
+--create new key with second and rand value into recurse call
+doMarkov :: MarkovChain -> Markov -> Integer -> IO String
+doMarkov (x:xs) m count = prntMarkov x
 
 main :: IO ()
 main = do
     contents <- getLine
-    print . grpMarkov . genMarkov $ words contents
+    let m = grpMarkov . genMarkov $ words contents
+    let first = head $ tail $ m
+    (doMarkov m first 0) >>= putStrLn :: IO ()
